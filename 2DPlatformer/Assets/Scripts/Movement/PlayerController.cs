7d8ad902d309eb8d812bf1b7e6ae0788c49caf92
Unknown <jcbelgard@gmail.com>
2019-04-08 11:56:02 -0500

    Jumping

---
+++ 2DPlatformer/Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float moveSpeed;
+    private Rigidbody2D myBody;
+
+    private float horizontal;
+    private float vertical;
+
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        MoveHorizontal();
+
+    }
+
+
+
+    private void Jump()
+    {
+        myBody.AddForce(Vector2.up * jumpForce);
+    }
+
+
+    private void MoveHorizontal()
+    {
+        if (horizontal != 0f)
+        {
+            myBody.velocity = new Vector2(horizontal * moveSpeed, myBody.velocity.y);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
