881f036ac6ea7a96bb41d8d0ec807efa53fb8e64
Unknown <jcbelgard@gmail.com>
2019-04-08 13:24:45 -0500

    Prelim Anim stuff

---
+++ 2DPlatformer/Assets/Scripts/Animation/AnimHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimHelper : MonoBehaviour
+{
+
+    private Animator myAnim;
+
+
+
+    private void Awake()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+}
