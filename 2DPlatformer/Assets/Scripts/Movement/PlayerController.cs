879d3e404448d0cc4a1d142b8a94b5eb7685d2a5
Unknown <jcbelgard@gmail.com>
2019-04-09 15:06:45 -0500

    Dash and alt jump

--- 2DPlatformer/Assets/Scripts/Movement/PlayerController.cs
+++ 2DPlatformer/Assets/Scripts/Movement/PlayerController.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
+    public enum JumpType {
+        Standard,
+        Variable
+    }
+
+    public enum MoveState {
+        Standing,
+        Running,
+        Dashing
+    }
+
+    public JumpType jumpType = JumpType.Standard;
     [Header("Temp Jump Variables")]
     public float jumpForce = 1200f;
     public float arialJumpForce = 500f;
     public float moveSpeed;
     public int maxJumpCount = 1;
+
+    [Header("Variable Jump Variables")]
+    public float descendingFallMod = 1.5f;
+    public float ascendingFallMod = 1f;
+
+    [Header("Dash Variables")]
+    public float dashDuration = 0.5f;
+    public float dashSpeed = 30f;
+    public float dashCooldown = 3f;
+
 
     [Header("Layer Masks")]
     public LayerMask groundLayer;
@@ -20,6 +41,10 @@ public class PlayerController : MonoBehaviour
     [Header("Flags")]
     public bool isGrounded;
 
+    private bool usingDash;
+
+    [Space(20)]
+    public MoveState moveState = MoveState.Standing;
 
     private Rigidbody2D myBody;
     private AnimHelper animHelper;
@@ -27,6 +52,9 @@ public class PlayerController : MonoBehaviour
     private float horizontal;
     private float vertical;
     private int currentJumpCount;
+
+    private Timer dashTimer;
+    private Timer dashCooldownTimer;
 
 
     private void Awake()
@@ -36,18 +64,38 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    private void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        dashTimer = new Timer(dashDuration, EndDash, true);
+        dashCooldownTimer = new Timer(dashCooldown, RefreshDash, true);
+    }
+
 
     private void Update()
     {
-         horizontal = Input.GetAxis("Horizontal");
-         vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        UpdateDash();
+        CheckMoveState();
 
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
 
-        //ResetJump();
+        if (Input.GetButtonDown("Dash"))
+        {
+            BeginDash();
+        }
+
+        if (jumpType == JumpType.Variable)
+            VariableFall();
     }
 
 
@@ -57,15 +105,11 @@ public class PlayerController : MonoBehaviour
         CheckGround();
     }
 
-
-
     private void Jump()
     {
-
         float desiredJumpForce = isGrounded ? jumpForce : arialJumpForce;
 
-
-        if(currentJumpCount >= maxJumpCount)
+        if (currentJumpCount >= maxJumpCount)
         {
             return;
         }
@@ -81,22 +125,106 @@ public class PlayerController : MonoBehaviour
 
     }
 
-
-    private void MoveHorizontal()
+    private void VariableFall()
     {
+        Vector2 desiredFallVelocity = Vector2.zero;
+
+        if (myBody.velocity.y < 0)
+        {
+            desiredFallVelocity = Vector2.up * Physics2D.gravity.y * descendingFallMod * Time.deltaTime;
+        }
+        else if (myBody.velocity.y > 0 && Input.GetButton("Jump") == false)
+        {
+            desiredFallVelocity = Vector2.up * Physics2D.gravity.y * ascendingFallMod * Time.deltaTime;
+        }
+
+        myBody.velocity += desiredFallVelocity;
+    }
+
+
+
+    private void CheckMoveState()
+    {
+
+        if (moveState == MoveState.Dashing)
+        {
+            return;
+        }
+
         if (horizontal != 0f)
         {
-            SetFacing();
-
-            if(isGrounded)
-                animHelper.PlayWalk();
-
-            myBody.velocity = new Vector2(horizontal * moveSpeed, myBody.velocity.y);
+            SetMoveState(MoveState.Running);
         }
         else
         {
-            animHelper.StopWalk();
+            SetMoveState(MoveState.Standing);
         }
+    }
+
+    private void MoveHorizontal()
+    {
+        float desiredSpeed = 0f;
+
+        switch (moveState)
+        {
+            case MoveState.Standing:
+
+                animHelper.StopWalk();
+
+                break;
+
+            case MoveState.Running:
+                SetFacing();
+                desiredSpeed = moveSpeed;
+
+                if (isGrounded)
+                    animHelper.PlayWalk();
+
+                break;
+
+            case MoveState.Dashing:
+                SetFacing();
+                desiredSpeed = dashSpeed;
+
+                break;
+        }
+
+        myBody.velocity = new Vector2(horizontal * desiredSpeed, myBody.velocity.y);
+
+
+    }
+
+
+
+
+    private void UpdateDash()
+    {
+        if (dashCooldownTimer != null && usingDash == true)
+            dashCooldownTimer.UpdateClock();
+
+
+        if (dashTimer != null && moveState == MoveState.Dashing)
+            dashTimer.UpdateClock();
+    }
+
+    private void BeginDash()
+    {
+        if (usingDash == true)
+            return;
+
+        usingDash = true;
+        SetMoveState(MoveState.Dashing);
+    }
+
+    private void EndDash()
+    {
+        SetMoveState(MoveState.Standing);
+    }
+
+    private void RefreshDash()
+    {
+        Debug.Log("Refreshing Dash");
+        usingDash = false;
     }
 
     private void CheckGround()
@@ -111,7 +239,6 @@ public class PlayerController : MonoBehaviour
             currentJumpCount = 0;
             animHelper.PlayOrStopAnimBool("Jumping", false);
         }
-
     }
 
 
@@ -124,28 +251,28 @@ public class PlayerController : MonoBehaviour
             return;
 
         ResetJump();
-
-
     }
 
 
     private void SetFacing()
     {
-        if (horizontal < 0)
+        if (horizontal < 0 && spriteRenderer.flipX == false)
         {
-            if (spriteRenderer.flipX == true)
-                return;
-
             spriteRenderer.flipX = true;
         }
 
-        if(horizontal > 0)
+        if (horizontal > 0 && spriteRenderer.flipX == true)
         {
-            if (spriteRenderer.flipX == false)
-                return;
-
             spriteRenderer.flipX = false;
         }
+    }
+
+    private void SetMoveState(MoveState state)
+    {
+        //Debug.Log(state + " is being set");
+
+        if (moveState != state)
+            moveState = state;
     }
 
 
---
+++ 2DPlatformer/Assets/Scripts/Tools/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer
+{
+
+    public float Ratio { get { return _timer / Duration; } }
+
+    public float Duration { get; private set; }
+    private bool resetTimerOnComplete;
+
+    private Action completionCallback;
+
+    private float _timer;
+
+
+    public Timer(float duration, Action completionCallback, bool resetOnComplete = false)
+    {
+        Duration = duration;
+        resetTimerOnComplete = resetOnComplete;
+
+        if (completionCallback != null)
+            this.completionCallback += completionCallback;
+    }
+
+    public void ModifyDuration(float mod)
+    {
+        Duration += mod;
+
+        if (Duration <= 0f)
+        {
+            Duration = 0f;
+        }
+
+        if (_timer > Duration)
+        {
+            _timer = 0f;
+        }
+    }
+
+    public void UpdateClock()
+    {
+        if (_timer < Duration)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer >= Duration)
+            {
+
+                if (completionCallback != null)
+                    completionCallback();
+
+                if (resetTimerOnComplete)
+                {
+                    ResetTimer();
+                }
+            }
+        }
+    }
+
+    public void ResetTimer()
+    {
+        _timer = 0f;
+    }
+}
